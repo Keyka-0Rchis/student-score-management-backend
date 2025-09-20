0d00098dd5731c5bc2834211d3e63ab0d9d35aef
Keyka-0Rchis <tkwsm1221@gmail.com>
2025-09-20 19:05:06 +0900

    役職のモデルを修正

---
+++ src/main/java/works/keyka/model/RoleModel.java
@@ -0,0 +1,9 @@
+package works.keyka.model;
+
+import lombok.Data;
+
+@Data
+public class RoleModel {
+	private int roleID;
+	private String role;
+}
--- src/main/java/works/keyka/model/RollModel.java
+++
@@ -1,5 +0,0 @@
-package works.keyka.model;
-
-public class RollModel {
-
-}
