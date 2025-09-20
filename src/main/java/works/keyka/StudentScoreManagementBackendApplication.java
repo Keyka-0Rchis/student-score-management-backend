a4640609d7fe2f4c04582ee1ed0346177feee33d
Keyka-0Rchis <tkwsm1221@gmail.com>
2025-09-20 15:00:05 +0900

    first_commit

---
+++ src/main/java/works/keyka/StudentScoreManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package works.keyka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentScoreManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentScoreManagementBackendApplication.class, args);
+	}
+
+}
